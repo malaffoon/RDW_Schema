50ebc2d12deeb7b55c9924d2e1d1332b15e5d682
Alla Gorina <agorina@fairwaytech.com>
2017-05-05 07:47:47 -0700

    SQL to migrate asmt.

--- staging/sql/V201702061486427077__initial_ddl.sql
+++ staging/sql/V201702061486427077__initial_ddl.sql
@@ -6,6 +6,8 @@ ALTER DATABASE `${schemaName}` CHARACTER SET utf8  COLLATE utf8_unicode_ci;
 
 USE `${schemaName}`;
 
+/** School and District **/
+
 CREATE TABLE IF NOT EXISTS staging_district (
   id int NOT NULL PRIMARY KEY,
   natural_id varchar(40) NOT NULL,
@@ -22,6 +24,8 @@ CREATE TABLE IF NOT EXISTS staging_school (
   deleted tinyint NOT NULL,
   migrate_id bigint NOT NULL
 );
+
+/** Student **/
 
 CREATE TABLE IF NOT EXISTS staging_student (
   id int NOT NULL PRIMARY KEY,
@@ -43,4 +47,45 @@ CREATE TABLE IF NOT EXISTS staging_student_ethnicity (
   id int NOT NULL PRIMARY KEY,
   ethnicity_id tinyint NOT NULL,
   student_id int NOT NULL
+);
+
+/** Assessment Packages related data **/
+
+CREATE TABLE IF NOT EXISTS staging_asmt (
+  id int NOT NULL PRIMARY KEY,
+  natural_id varchar(250) NOT NULL,
+  grade_id tinyint NOT NULL,
+  type_id tinyint NOT NULL,
+  subject_id tinyint NOT NULL,
+  school_year smallint NOT NULL,
+  name varchar(250),
+  label varchar(255),
+  version varchar(30),
+  import_id bigint NOT NULL,
+  deleted tinyint NOT NULL DEFAULT 0,
+  migrate_id bigint NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS staging_asmt_score (
+  asmt_id int NOT NULL PRIMARY KEY,
+  cut_point_1 float NOT NULL,
+  cut_point_2 float NOT NULL,
+  cut_point_3 float NOT NULL,
+  min_score float NOT NULL,
+  max_score float NOT NULL,
+  migrate_id bigint NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS staging_item (
+  id int NOT NULL PRIMARY KEY,
+  claim_id smallint,
+  target_id smallint,
+  natural_id varchar(40) NOT NULL,
+  asmt_id int NOT NULL,
+  math_practice tinyint,
+  allow_calc tinyint,
+  dok_id tinyint NOT NULL,
+  difficulty float NOT NULL,
+  max_points float UNSIGNED NOT NULL,
+  migrate_id bigint NOT NULL
 );
