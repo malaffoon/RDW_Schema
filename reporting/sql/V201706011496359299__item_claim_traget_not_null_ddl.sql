752c3803214d8bc1f16a6867d6551d5685a9cffd
Alla Gorina <agorina@fairwaytech.com>
2017-06-05 19:04:50 -0700

    Core review feedback addressed.

--- reporting/sql/V201706011496359299__item_claim_traget_not_null_ddl.sql
+++ reporting/sql/V201706011496359299__item_claim_traget_not_null_ddl.sql
@@ -1,8 +1,15 @@
 /**
-* change migrate update column to be refreshed on update
+* change item primary claim and target to be not null
 **/
 
 USE ${schemaName};
+
+-- please note that this is not instantenious, expect some delay..in seconds
+UPDATE item i
+  JOIN asmt a
+SET
+  i.claim_id = (select id from claim c where c.subject_id = a.subject_id limit 1),
+  i.target_id = (select id from target t where t.claim_id = (select id from claim where subject_id = a.subject_id limit 1) limit 1) ;
 
 ALTER TABLE item MODIFY COLUMN claim_id smallint NOT NULL;
 ALTER TABLE item MODIFY COLUMN target_id smallint NOT NULL;
