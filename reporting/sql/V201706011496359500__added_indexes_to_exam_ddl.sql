7e6610f3a39faf7e180ba161e7796c149ffd0815
Alla Gorina <agorina@fairwaytech.com>
2017-06-16 17:31:07 -0700

    Removed FK index.

--- reporting/sql/V201706011496359500__added_indexes_to_exam_ddl.sql
+++ reporting/sql/V201706011496359500__added_indexes_to_exam_ddl.sql
@@ -7,6 +7,3 @@ USE ${schemaName};
 CREATE INDEX idx__exam__asmt_school_year ON exam (asmt_id, school_id, school_year);
 CREATE INDEX idx__exam__school_grade ON exam (school_id, grade_id);
 
--- When adding an index that has the first column from a FOREIGN KEY CONSTRAINT, mySQL drops the FK index
--- This index seems to still improve performance of some queries, so I am adding it back.
-CREATE INDEX fk__exam__school ON exam (school_id);
