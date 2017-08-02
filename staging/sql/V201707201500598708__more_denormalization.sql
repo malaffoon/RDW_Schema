d46a4948e8e12ecf2b5712b38e25634fe79d5e68
Alla Gorina <agorina@fairwaytech.com>
2017-08-01 19:48:00 -0700

    DWR-689 Merge reporting schemas

--- staging/sql/V201707201500598708__more_denormalization.sql
+++
@@ -1,7 +0,0 @@
-# add code to math_practice
-
-USE ${schemaName};
-
-ALTER TABLE staging_exam ADD COLUMN available_accommodation_codes VARCHAR(500);
-
-ALTER TABLE staging_math_practice ADD COLUMN code VARCHAR(4) NOT NULL;
--- staging/sql/V201707221500770108__remove_exam_acc_codes.sql
+++
@@ -1,6 +0,0 @@
-# remove available_accommodation_codes from exam in staging
-
-USE ${schemaName};
-
-ALTER TABLE staging_exam DROP COLUMN available_accommodation_codes;
-
