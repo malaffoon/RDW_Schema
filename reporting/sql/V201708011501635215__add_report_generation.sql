c21a2d404e745e9bd15f99dd989190355691bd97
wtritch <wtritchler@fairwaytech.com>
2017-08-02 09:20:49 -0700

    Accounting for code review comments.

--- reporting/sql/V201708011501635215__add_report_generation.sql
+++ reporting/sql/V201708011501635215__add_report_generation.sql
@@ -2,15 +2,14 @@
 
 USE ${schemaName};
 
-CREATE TABLE IF NOT EXISTS report_generation (
+CREATE TABLE IF NOT EXISTS user_report (
   id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_login varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  user_login varchar(255) NOT NULL,
   job_execution_id bigint(20),
   status tinyint(4) NOT NULL,
   report_resource_uri varchar(255),
   label varchar(255) NOT NULL,
   report_request text NOT NULL,
   created TIMESTAMP(6) NOT NULL,
-  KEY idx__report_generation__user_login_label (user_login, label),
-  KEY idx__report_generation__created (created)
+  INDEX idx__report_generation__user_login (user_login)
 );
