9a60fbba9bcef0e5495071939903eaf6acf846ee
wtritch <wtritchler@fairwaytech.com>
2017-08-21 17:29:27 -0700

    Update the upload student_id column to match the student.id data type

--- warehouse/sql/V201708211503357022__update_group_processing_indexes.sql
+++ warehouse/sql/V201708211503357022__update_group_processing_indexes.sql
@@ -1,4 +1,5 @@
 -- Update the unique index to consider the school id
+-- Modify the upload student_id column to match the student.id type
 
 USE ${schemaName};
 
@@ -7,3 +8,6 @@ ALTER TABLE upload_student_group_import
 
 ALTER TABLE upload_student_group_import
   ADD UNIQUE INDEX idx__upload_student_group_import__batch_ref_school (batch_id, ref_type, school_id);
+
+ALTER TABLE upload_student_group
+  MODIFY COLUMN student_id int(11);
