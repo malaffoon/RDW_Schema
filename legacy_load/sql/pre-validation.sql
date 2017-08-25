b1d23a3e769be4b2434827a8e11d48e4b709ae05
Alla Gorina <agorina@fairwaytech.com>
2017-08-24 20:40:28 -0700

    DWR-503 First draft of the Postgres counts for validation - round 2.

--- legacy_load/sql/pre-validation.sql
+++ legacy_load/sql/pre-validation.sql
@@ -1,20 +1,45 @@
 set SEARCH_PATH to edware_ca;
 
-select count(*) from fact_asmt_outcome_vw where rec_status = 'C';
-select count(*) from fact_block_asmt_outcome where rec_status = 'C';
+create table pre_validation
+(
+    testNum int,
+	result1 varchar(1000),
+	result2 varchar(1000),
+	result3 varchar(1000),
+	result4 varchar(1000),
+	result5 varchar(1000),
+	result6 varchar(1000),
+	result7 varchar(1000),
+	result8 varchar(1000),
+	created timestamp default current_timestamp
+);
 
-SELECT
+insert into pre_validation select 1, 'total_ica';
+insert into pre_validation select (select max(testNum) from pre_validation), count(*)  from fact_asmt_outcome_vw where rec_status = 'C';
+
+insert into pre_validation select (select max(testNum) from pre_validation) + 1, 'total_iab';
+insert into pre_validation select (select max(testNum) from pre_validation), count(*) from fact_block_asmt_outcome where rec_status = 'C';
+
+insert into pre_validation select (select max(testNum) from pre_validation) + 1, 'total ica score', 'total std err', 'total perf level';
+insert into pre_validation select (select max(testNum) from pre_validation),
   sum(asmt_score)                        AS total_score,
   sum(asmt_score_range_max - asmt_score) AS total_std_err,
   sum(asmt_perf_lvl)                     AS total_perf_level
 FROM fact_asmt_outcome_vw
 WHERE rec_status = 'C';
 
-select count(distinct student_id) from dim_student where rec_status = 'C';
-select count(distinct student_id) from fact_asmt_outcome_vw where rec_status = 'C';
-select count(distinct student_id) from fact_block_asmt_outcome where rec_status = 'C';
 
-SELECT
+insert into pre_validation select (select max(testNum) from pre_validation) + 1, 'total students';
+insert into pre_validation select (select max(testNum) from pre_validation), count(distinct student_id) from dim_student where rec_status = 'C';
+
+insert into pre_validation select (select max(testNum) from pre_validation) + 1, 'total students with ica exams';
+insert into pre_validation select (select max(testNum) from pre_validation), count(distinct student_id) from fact_asmt_outcome_vw where rec_status = 'C';
+
+insert into pre_validation select (select max(testNum) from pre_validation) + 1, 'total students with iab exams';
+insert into pre_validation select (select max(testNum) from pre_validation), count(distinct student_id) from fact_block_asmt_outcome where rec_status = 'C';
+
+insert into pre_validation select (select max(testNum) from pre_validation) + 1, 'ica exams', 'asmt', 'asmt year', 'admin condition', 'complete';
+insert into pre_validation select (select max(testNum) from pre_validation),
   count(*),
   asmt_guid,
   asmt_year,
@@ -25,7 +50,8 @@ WHERE rec_status = 'C'
 GROUP BY asmt_guid, asmt_year, administration_condition, complete
 ORDER BY asmt_year, asmt_guid,administration_condition, complete;
 
-SELECT
+insert into pre_validation select (select max(testNum) from pre_validation) + 1, 'iab exams', 'asmt', 'asmt year', 'admin condition', 'complete';
+insert into pre_validation select (select max(testNum) from pre_validation),
   count(*),
   asmt_guid,
   asmt_year,
@@ -36,7 +62,8 @@ WHERE rec_status = 'C'
 GROUP BY asmt_guid, asmt_year, administration_condition, complete
 ORDER BY asmt_year, asmt_guid,administration_condition, complete;
 
-SELECT
+insert into pre_validation select (select max(testNum) from pre_validation) + 1, 'ica exams', 'district', 'school';
+insert into pre_validation select (select max(testNum) from pre_validation),
   s.*,
   h.district_name,
   h.school_name
@@ -52,7 +79,8 @@ FROM (
   JOIN dim_inst_hier h ON h.school_id = s.school_id
 ORDER BY district_name, school_name, asmt_year;
 
-SELECT
+insert into pre_validation select (select max(testNum) from pre_validation) + 1, 'iab exams', 'district', 'school';
+insert into pre_validation select (select max(testNum) from pre_validation),
   s.*,
   h.district_name,
   h.school_name
