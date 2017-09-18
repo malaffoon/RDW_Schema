cc4dd17799bf2c033754e5c8416414f96d40e921
wtritch <wtritchler@fairwaytech.com>
2017-09-18 09:42:21 -0700

    Do no remove the job_execution_id column until changes are fully deployed.

--- reporting/sql/V1_1_0_1__add_user_report_chunk_tracking.sql
+++ reporting/sql/V1_1_0_1__add_user_report_chunk_tracking.sql
@@ -1,6 +1,5 @@
--- Modify user_report table to track chunk completion and remove unused job_execution_id column
+-- Modify user_report table to track chunk completion
 USE ${schemaName};
 
-ALTER TABLE user_report DROP COLUMN job_execution_id;
 ALTER TABLE user_report ADD COLUMN total_chunk_count INT NOT NULL DEFAULT 0;
 ALTER TABLE user_report ADD COLUMN complete_chunk_count INT NOT NULL DEFAULT 0;
