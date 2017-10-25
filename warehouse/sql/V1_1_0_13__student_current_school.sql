f325105f0dc73b23ada3fa1a132ac7b8e6bfc7f0
Alla Gorina <agorina@fairwaytech.com>
2017-10-25 16:53:19 -0700

    DWR-1104 Fix import/migrate.

--- warehouse/sql/V1_1_0_13__student_current_school.sql
+++ warehouse/sql/V1_1_0_13__student_current_school.sql
@@ -1,7 +1,6 @@
 USE ${schemaName};
 
 ALTER TABLE student
-  MODIFY COLUMN updated TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   ADD COLUMN inferred_effective_date TIMESTAMP(6) DEFAULT NULL,
   ADD COLUMN inferred_school_id INT,
   ADD COLUMN partition_id INT,
@@ -12,6 +11,9 @@ SET @student_partition_end = 100;
 
 UPDATE student s SET s.partition_id = MOD(s.id, @student_partition_end + 1);
 
+INSERT INTO import (status, content, contentType, digest, batch)
+ SELECT DISTINCT 0, 4, 'missing legacy schools', 'missing legacy schools', partition_id from student;
+
 -- Helper to run partitions in a loop
 DELIMITER //
 CREATE PROCEDURE loop_by_partition(IN p_sql VARCHAR(1000), IN p_first INTEGER, IN p_last INTEGER)
@@ -21,7 +23,7 @@ CREATE PROCEDURE loop_by_partition(IN p_sql VARCHAR(1000), IN p_first INTEGER, I
 
     partition_loop: LOOP
 
-      SET @stmt = concat(p_sql, ' and partition_id =', iteration);
+      SET @stmt = concat(p_sql, ' and s.partition_id =', iteration);
       PREPARE stmt FROM @stmt;
       EXECUTE stmt;
       DEALLOCATE PREPARE stmt;
@@ -42,12 +44,14 @@ DELIMITER ;
 -- NOTE that this does not account for DELETED exams assuming that we do not have a case when deleted exam has the most recent completed_at
 CALL loop_by_partition(
     'UPDATE student s
+      JOIN (SELECT id, cast(batch AS UNSIGNED) partition_id FROM import) i ON s.partition_id = i.partition_id
       JOIN exam AS e1 ON s.id = e1.student_id
       LEFT OUTER JOIN exam AS e2 ON e1.student_id = e2.student_id
            AND (e1.completed_at < e2.completed_at OR (e1.completed_at = e2.completed_at AND e1.Id < e2.Id))
     SET
       s.inferred_effective_date = e1.completed_at,
-      s.inferred_school_id      = e1.school_id
+      s.inferred_school_id      = e1.school_id,
+      s.update_import_id        = i.id
     WHERE e2.student_id IS NULL ',
     @student_partition_start, @student_partition_end);
 
@@ -55,8 +59,9 @@ DROP PROCEDURE loop_by_partition;
 
 ALTER TABLE student
   DROP INDEX idx__student__partition_id,
-  DROP COLUMN partition_id,
-  MODIFY COLUMN updated TIMESTAMP (6) NOT NULL DEFAULT CURRENT_TIMESTAMP (6) ON UPDATE CURRENT_TIMESTAMP(6);
+  DROP COLUMN partition_id;
+
+UPDATE import SET status = 1 WHERE contentType = 'missing legacy schools';
 
 /************************************* Stored procedures ***************************************/
 
