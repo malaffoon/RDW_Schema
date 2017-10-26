12d3e9a279f0116ead40d4ed65a5bc8db2442769
Alla Gorina <agorina@fairwaytech.com>
2017-10-25 19:55:37 -0700

    DWR-1104 Changed initial load to adhere to the same rules as the initial load of historical data

--- warehouse/sql/V1_1_0_14__initial_load_of_current_school.sql
+++ warehouse/sql/V1_1_0_14__initial_load_of_current_school.sql
@@ -1,16 +1,18 @@
 USE ${schemaName};
 
 ALTER TABLE student
+  MODIFY COLUMN updated TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
   ADD COLUMN partition_id INT,
   ADD INDEX idx__student__partition_id (partition_id);
 
 SET @student_partition_start = 0;
-SET @student_partition_end = 100;
+SET @student_partition_end = 99;
 
 UPDATE student s SET s.partition_id = MOD(s.id, @student_partition_end + 1);
 
+-- one import record per student
 INSERT INTO import (status, content, contentType, digest, batch)
- SELECT DISTINCT 0, 4, 'missing legacy schools', 'missing legacy schools', partition_id from student;
+  SELECT 0, 1, 'initial load of inferred schools', 'initial load of inferred schools', id from student;
 
 -- Helper to run partitions in a loop
 DELIMITER //
@@ -42,7 +44,7 @@ DELIMITER ;
 -- NOTE that this does not account for DELETED exams assuming that we do not have a case when deleted exam has the most recent completed_at
 CALL loop_by_partition(
     'UPDATE student s
-      JOIN (SELECT id, cast(batch AS UNSIGNED) partition_id FROM import) i ON s.partition_id = i.partition_id
+      JOIN (SELECT id, cast(batch AS UNSIGNED) student_id FROM import where digest = ''initial load of inferred schools'' and status = 0) i ON s.id = i.student_id
       JOIN exam AS e1 ON s.id = e1.student_id
       LEFT OUTER JOIN exam AS e2 ON e1.student_id = e2.student_id
            AND (e1.completed_at < e2.completed_at OR (e1.completed_at = e2.completed_at AND e1.Id < e2.Id))
@@ -55,8 +57,27 @@ CALL loop_by_partition(
 
 DROP PROCEDURE loop_by_partition;
 
+-- distribute imports for migrate to have smaller chunks
+UPDATE import
+SET status = 1,
+  created = DATE_ADD(created, INTERVAL (@maxImportId -id)  MICROSECOND),
+  updated = DATE_ADD(updated, INTERVAL (@maxImportId -id)  MICROSECOND)
+WHERE status = 0
+      and content = 1
+      and digest = 'initial load of inferred schools';
+
+-- update date to match imports
+UPDATE student s
+  JOIN import i ON i.id = s.update_import_id
+SET
+  s.updated = i.updated
+WHERE i.status = 1
+      and content = 1
+      and digest = 'initial load of inferred schools';
+
+-- revert temporary changes
 ALTER TABLE student
+  MODIFY COLUMN updated TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   DROP INDEX idx__student__partition_id,
   DROP COLUMN partition_id;
 
-UPDATE import SET status = 1 WHERE contentType = 'missing legacy schools';
