5b9c54c3c97dacd8be7028eacae99ab6106612b6
Alla Gorina <agorina@fairwaytech.com>
2017-10-27 09:20:03 -0700

    DWR-1067 Renamed number_of_answer_options.

--- reporting/sql/V1_1_0_8__additional_item_info.sql
+++ reporting/sql/V1_1_0_8__additional_item_info.sql
@@ -4,12 +4,12 @@ ALTER TABLE item
     ADD COLUMN field_test tinyint,
     ADD COLUMN active tinyint,
     ADD COLUMN type varchar(40),
-    ADD COLUMN answer_options tinyint,
+    ADD COLUMN number_of_answer_options tinyint,
     ADD COLUMN answer_key varchar(50);
 
 ALTER TABLE staging_item
     ADD COLUMN field_test tinyint,
     ADD COLUMN active tinyint,
     ADD COLUMN type varchar(40),
-    ADD COLUMN answer_options tinyint,
+    ADD COLUMN number_of_answer_options tinyint,
     ADD COLUMN answer_key varchar(50);
--- warehouse/sql/V1_1_0_15__additional_item_info.sql
+++ warehouse/sql/V1_1_0_15__additional_item_info.sql
@@ -1,5 +1,5 @@
 USE ${schemaName};
 
 ALTER TABLE item
-    ADD COLUMN answer_options tinyint,
+    ADD COLUMN number_of_answer_options tinyint,
     ADD COLUMN answer_key varchar(50);
