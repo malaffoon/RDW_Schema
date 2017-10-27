746ad04a4239ee4d0feeb2d529bcee33243c67a1
Alla Gorina <agorina@fairwaytech.com>
2017-10-27 09:08:42 -0700

    DWR-1067 Change answer data type and name.

--- reporting/sql/V1_1_0_8__additional_item_info.sql
+++ reporting/sql/V1_1_0_8__additional_item_info.sql
@@ -5,11 +5,11 @@ ALTER TABLE item
     ADD COLUMN active tinyint,
     ADD COLUMN type varchar(40),
     ADD COLUMN answer_options tinyint,
-    ADD COLUMN answer text;
+    ADD COLUMN answer_key varchar(50);
 
 ALTER TABLE staging_item
     ADD COLUMN field_test tinyint,
     ADD COLUMN active tinyint,
     ADD COLUMN type varchar(40),
     ADD COLUMN answer_options tinyint,
-    ADD COLUMN answer text;
+    ADD COLUMN answer_key varchar(50);
--- warehouse/sql/V1_1_0_15__additional_item_info.sql
+++ warehouse/sql/V1_1_0_15__additional_item_info.sql
@@ -2,4 +2,4 @@ USE ${schemaName};
 
 ALTER TABLE item
     ADD COLUMN answer_options tinyint,
-    ADD COLUMN answer text;
+    ADD COLUMN answer_key varchar(50);
