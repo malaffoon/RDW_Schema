e9da5f714af60cc6493fb60c3730bfda68c3a339
esotrope <llunsford@fairwaytech.com>
2017-11-27 15:51:13 -0800

    changes index and fk names to match convention

--- warehouse/sql/V1_1_0_22__embargo.sql
+++ warehouse/sql/V1_1_0_22__embargo.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS district_embargo (
   updated_by varchar(128),
   PRIMARY KEY(district_id, embargo_scope_id, school_year, updated_on),
   INDEX idx__district_embargo__district (district_id),
-  INDEX idx__district_embargo__scope (embargo_scope_id),
+  INDEX idx__district_embargo__embargo_scope (embargo_scope_id),
   CONSTRAINT fk__district_embargo__district FOREIGN KEY (district_id) REFERENCES district(id) ON DELETE CASCADE,
   CONSTRAINT fk__district_embargo__embargo_scope FOREIGN KEY (embargo_scope_id) REFERENCES embargo_scope(id)
 );
@@ -32,6 +32,6 @@ CREATE TABLE IF NOT EXISTS state_embargo (
   updated_on TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) NOT NULL,
   updated_by varchar(128),
   PRIMARY KEY(embargo_scope_id, school_year, updated_on),
-  INDEX idx__state_embargo__scope (embargo_scope_id),
+  INDEX idx__state_embargo__embargo_scope (embargo_scope_id),
   CONSTRAINT fk__state_embargo__embargo_scope FOREIGN KEY (embargo_scope_id) REFERENCES embargo_scope(id)
 );
