0261be5083d568c456ee9adfdaf854a0ed5a3982
esotrope <llunsford@fairwaytech.com>
2017-11-28 09:52:21 -0800

    accounts for review comments

--- reporting/sql/V1_1_0_14__embargo.sql
+++ reporting/sql/V1_1_0_14__embargo.sql
@@ -4,10 +4,6 @@ ALTER TABLE school ADD district_embargo_enabled tinyint;
 UPDATE school SET district_embargo_enabled = 0;
 ALTER TABLE school MODIFY COLUMN district_embargo_enabled tinyint NOT NULL;
 
-ALTER TABLE school_group ADD district_embargo_enabled tinyint;
-UPDATE school_group SET district_embargo_enabled = 0;
-ALTER TABLE school_group MODIFY COLUMN district_embargo_enabled tinyint NOT NULL;
-
 ALTER TABLE district ADD embargo_enabled tinyint;
 UPDATE district SET embargo_enabled = 0;
 ALTER TABLE district MODIFY COLUMN embargo_enabled tinyint NOT NULL;
