d2e50f61882cfae9915a890f55bf52d119d830f7
esotrope <llunsford@fairwaytech.com>
2017-11-27 17:10:58 -0800

    changed column names and adjusted for tables that already have data

--- reporting/sql/V1_1_0_14__embargo.sql
+++ reporting/sql/V1_1_0_14__embargo.sql
@@ -1,10 +1,13 @@
 USE ${schemaName};
 
-ALTER TABLE school
-  ADD embargoed tinyint NOT NULL;
+ALTER TABLE school ADD district_embargo_enabled tinyint;
+UPDATE school SET district_embargo_enabled = 0;
+ALTER TABLE school MODIFY COLUMN district_embargo_enabled tinyint NOT NULL;
 
-ALTER TABLE school_group
-  ADD embargoed tinyint NOT NULL;
+ALTER TABLE school_group ADD district_embargo_enabled tinyint;
+UPDATE school_group SET district_embargo_enabled = 0;
+ALTER TABLE school_group MODIFY COLUMN district_embargo_enabled tinyint NOT NULL;
 
-ALTER TABLE district
-  ADD embargoed tinyint NOT NULL;
+ALTER TABLE district ADD embargo_enabled tinyint;
+UPDATE district SET embargo_enabled = 0;
+ALTER TABLE district MODIFY COLUMN embargo_enabled tinyint NOT NULL;
