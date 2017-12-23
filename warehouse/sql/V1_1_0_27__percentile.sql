311d0e9b64cd81cff575a0b76ac15e4326040792
Chris Dietz <cdietz@fairwaytech.com>
2017-12-22 18:05:52 -0800

    percentile   - name change: change number to count   - make score a float as it is baed on scale_score that is a float   - fix missing `on update` for `updated` timestamp percentile_score   - make compound primary key (any issue with float part of key?)   - add contraint to parent table   - remove calculated min and max, will add to reporting schema.

--- warehouse/sql/V1_1_0_27__percentile.sql
+++ warehouse/sql/V1_1_0_27__percentile.sql
@@ -5,23 +5,21 @@ USE ${schemaName};
 CREATE TABLE percentile (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   asmt_id INT NOT NULL,
-  number INT NOT NULL,
+  count INT NOT NULL,
   mean FLOAT NOT NULL,
   standard_deviation FLOAT NULL,
   import_id BIGINT NOT NULL,
   update_import_id BIGINT NOT NULL,
-  created TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) NOT NULL,
-  updated TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP(6) NOT NULL,
-  CONSTRAINT fk__percentile__asmt
-    FOREIGN KEY (asmt_id) REFERENCES asmt (id)
+  created TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  updated TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  CONSTRAINT fk__percentile__asmt FOREIGN KEY (asmt_id) REFERENCES asmt (id)
 );
 
 CREATE TABLE percentile_score (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   percentile_id INT NOT NULL,
   percent TINYINT NOT NULL,
-  score INT NOT NULL,
-  min INT NOT NULL,
-  max INT NOT NULL
+  score FLOAT NOT NULL,
+  PRIMARY KEY (percentile_id, percent),
+  CONSTRAINT fk__percentile_score__percentile FOREIGN KEY (percentile_id) REFERENCES percentile (id)
 );
 
