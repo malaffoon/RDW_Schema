2d353c4bb876230616725e1e807fbcee6b9a62f4
Chris Dietz <cdietz@fairwaytech.com>
2017-12-26 22:47:05 -0800

    change name of min and max  to min_score and max_score and keep in warehouse to perform calculation during ingest and validation of percentile.

--- warehouse/sql/V1_1_0_27__percentile.sql
+++ warehouse/sql/V1_1_0_27__percentile.sql
@@ -19,6 +19,8 @@ CREATE TABLE percentile_score (
   percentile_id INT NOT NULL,
   percent TINYINT NOT NULL,
   score FLOAT NOT NULL,
+  min_score SMALLINT NOT NULL,
+  max_score SMALLINT NOT NULL,
   PRIMARY KEY (percentile_id, percent),
   CONSTRAINT fk__percentile_score__percentile FOREIGN KEY (percentile_id) REFERENCES percentile (id)
 );
