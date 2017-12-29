3dabc25876941dfe8df27048b43b40b3f0870f71
Chris Dietz <cdietz@fairwaytech.com>
2017-12-29 08:47:59 -0800

    reporting and staging tables for percentile tables

---
+++ reporting/sql/V1_1_0_1__percentile.sql
@@ -0,0 +1,46 @@
+-- Add support for percentile
+
+USE ${schemaName};
+
+CREATE TABLE percentile (
+  id INT NOT NULL PRIMARY KEY,
+  asmt_id INT NOT NULL,
+  count INT NOT NULL,
+  mean FLOAT NOT NULL,
+  standard_deviation FLOAT NULL,
+  update_import_id BIGINT NOT NULL,
+  updated TIMESTAMP(6) NOT NULL,
+  migrate_id BIGINT NOT NULL,
+  CONSTRAINT fk__percentile__asmt FOREIGN KEY (asmt_id) REFERENCES asmt (id)
+);
+
+CREATE TABLE percentile_score (
+  percentile_id INT NOT NULL,
+  percent TINYINT NOT NULL,
+  score FLOAT NOT NULL,
+  min_score SMALLINT NOT NULL,
+  max_score SMALLINT NOT NULL,
+  PRIMARY KEY (percentile_id, percent),
+  CONSTRAINT fk__percentile_score__percentile FOREIGN KEY (percentile_id) REFERENCES percentile (id)
+);
+
+CREATE TABLE staging_percentile (
+  id INT NOT NULL PRIMARY KEY,
+  asmt_id INT NOT NULL,
+  count INT NOT NULL,
+  mean FLOAT NOT NULL,
+  standard_deviation FLOAT NULL,
+  update_import_id BIGINT NOT NULL,
+  updated TIMESTAMP(6) NOT NULL,
+  migrate_id BIGINT NOT NULL,
+  deleted TINYINT NOT NULL
+);
+
+CREATE TABLE staging_percentile_score (
+  percentile_id INT NOT NULL,
+  percent TINYINT NOT NULL,
+  score FLOAT NOT NULL,
+  min_score SMALLINT NOT NULL,
+  max_score SMALLINT NOT NULL,
+  PRIMARY KEY (percentile_id, percent)
+);
