2731632e4b0786137726372c7b6c6e004ac52415
esotrope <llunsford@fairwaytech.com>
2018-05-10 12:48:53 -0700

    adds teacher student group tables

---
+++ reporting/sql/V1_2_0_6__add_teacher_student_groups.sql
@@ -0,0 +1,26 @@
+-- Adds teacher student group related tables
+
+use ${schemaName};
+
+CREATE TABLE IF NOT EXISTS teacher_student_group (
+  id int NOT NULL PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  school_id int NOT NULL,
+  school_year smallint NOT NULL,
+  subject_id tinyint,
+  user_login varchar(255) NOT NULL,
+  UNIQUE INDEX idx__teacher_student_group__school_name_year (school_id, name, school_year),
+  INDEX idx__teacher_student_group__subject (subject_id),
+  CONSTRAINT fk__teacher_student_group__school FOREIGN KEY (school_id) REFERENCES school(id),
+  CONSTRAINT fk__teacher_student_group__subject FOREIGN KEY (subject_id) REFERENCES subject(id),
+  CONSTRAINT fk__teacher_student_group__school_year FOREIGN KEY (school_year) REFERENCES school_year(year)
+);
+
+CREATE TABLE IF NOT EXISTS teacher_student_group_membership (
+  teacher_student_group_id int NOT NULL,
+  student_id int NOT NULL,
+  UNIQUE INDEX idx__teacher_student_group_membership (teacher_student_group_id, student_id),
+  INDEX idx_teacher_student_group_membership__student (student_id),
+  CONSTRAINT fk__teacher_student_group_membership__student_group FOREIGN KEY (teacher_student_group_id) REFERENCES teacher_student_group(id),
+  CONSTRAINT fk__teacher_student_group_membership__student FOREIGN KEY (student_id) REFERENCES student(id)
+);
