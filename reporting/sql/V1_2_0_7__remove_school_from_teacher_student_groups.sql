c25676a2128676d6afb94ff03aeb1106f1c0c817
esotrope <llunsford@fairwaytech.com>
2018-05-16 10:55:02 -0700

    removes school id column from teacher student group table

---
+++ reporting/sql/V1_2_0_7__remove_school_from_teacher_student_groups.sql
@@ -0,0 +1,9 @@
+-- Removes the school column from the teacher student group table
+
+use ${schemaName};
+
+ALTER TABLE teacher_student_group
+  DROP CONSTRAINT fk__teacher_student_group__school;
+  DROP INDEX idx__teacher_student_group__school_name_year;
+  ADD UNIQUE INDEX idx__teacher_student_group__user_login_name_school_year (user_login, name, school_year);
+  DROP COLUMN school_id;
