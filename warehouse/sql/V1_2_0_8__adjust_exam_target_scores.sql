4c6e62b8778c4a932e9b58217aa9d92ca50de2bc
Alla Gorina <agorina@fairwaytech.com>
2018-05-29 13:31:45 -0700

    Fixing broken build.

--- warehouse/sql/V1_2_0_8__adjust_exam_target_scores.sql
+++ warehouse/sql/V1_2_0_8__adjust_exam_target_scores.sql
@@ -7,11 +7,13 @@ ALTER TABLE exam_target_score
   DROP FOREIGN KEY fk__exam_target_score__exam,
   -- drop indexes due to bad names
   DROP INDEX idx__exam_claim_score__exam,
-  DROP INDEX idx__exam_target_score_exam_target,
+  DROP INDEX idx__exam_target_score_exam_target;
+
+ALTER TABLE exam_target_score
   -- restore indexes and FKs
   ADD UNIQUE KEY idx__exam_target_score__exam_target(exam_id, target_id),
   ADD INDEX idx__exam_target_score__target(target_id),
   ADD CONSTRAINT fk__exam_target_score__exam FOREIGN KEY (exam_id) REFERENCES exam(id),
   ADD CONSTRAINT fk__exam_target_score__target FOREIGN KEY (target_id) REFERENCES target(id),
   -- remove unused column
-  DROP COLUMN performance_level;
+  DROP COLUMN   performance_level;
