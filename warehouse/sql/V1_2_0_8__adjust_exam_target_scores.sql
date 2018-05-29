9a9894b5804ef200e9ed06f8714a60adff155217
Alla Gorina <agorina@fairwaytech.com>
2018-05-29 11:31:13 -0700

    Clean up exam-target-score table.

---
+++ warehouse/sql/V1_2_0_8__adjust_exam_target_scores.sql
@@ -0,0 +1,17 @@
+-- Clean up indexes and drop unused column
+USE ${schemaName};
+
+ALTER TABLE exam_target_score
+ -- remove FK that use indexes to be modified
+  DROP FOREIGN KEY fk__exam_target_score__target,
+  DROP FOREIGN KEY fk__exam_target_score__exam,
+  -- drop indexes due to bad names
+  DROP INDEX idx__exam_claim_score__exam,
+  DROP INDEX idx__exam_target_score_exam_target,
+    -- restore indexes and FKs
+  ADD UNIQUE KEY idx__exam_target_score__exam_target(exam_id, target_id),
+  ADD INDEX idx__exam_target_score__target(target_id),
+  ADD CONSTRAINT fk__exam_target_score__exam FOREIGN KEY (exam_id) REFERENCES exam(id),
+  ADD CONSTRAINT fk__exam_target_score__target FOREIGN KEY (target_id) REFERENCES target(id),
+  -- remove unused column
+  DROP COLUMN performance_level;
