8487fce7d1924c23b758f6ace0400694d9232681
Alla Gorina <agorina@fairwaytech.com>
2018-06-03 09:46:51 -0700

    Code review feedback.

--- warehouse/sql/V1_2_0_11__trigger_migarte_summative_targets.sql
+++ warehouse/sql/V1_2_0_11__trigger_migarte_summative_targets.sql
@@ -1,12 +1,11 @@
 USE ${schemaName};
 
---TODO: NOTE remove this file from the production release
+-- NOTE: consider removing this file from the production release
 -- trigger migration of summative assessments to load asmt_target table in reporting data-mart
  INSERT INTO import (status, content, contentType, digest)
     SELECT 0, ic.id, 'force summative asmt migrate', left(uuid(), 8) from import_content ic where name = 'PACKAGE';
 
- SELECT max(id) INTO @import_id
-    FROM IMPORT WHERE contentType = 'force summative asmt migrate';
+ SELECT LAST_INSERT_ID() INTO @import_id;
 
  UPDATE asmt
     SET update_import_id = @import_id
