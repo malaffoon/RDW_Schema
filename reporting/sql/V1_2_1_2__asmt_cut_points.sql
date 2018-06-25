66ffc795ce1dbbeb91a646a24f27b9e1b6a924ed
Alla Gorina <agorina@fairwaytech.com>
2018-06-25 10:20:02 -0700

    Changed min/max to be required.

--- reporting/sql/V1_2_1_2__asmt_cut_points.sql
+++ reporting/sql/V1_2_1_2__asmt_cut_points.sql
@@ -4,23 +4,17 @@ use ${schemaName};
 ALTER TABLE asmt_score
  -- allow nulls
  MODIFY COLUMN cut_point_2 smallint,
- MODIFY COLUMN min_score smallint,
- MODIFY COLUMN max_score smallint,
  ADD COLUMN cut_point_4 smallint,
  ADD COLUMN cut_point_5 smallint;
 
 ALTER TABLE staging_asmt_score
 -- allow nulls
  MODIFY COLUMN cut_point_2 smallint,
- MODIFY COLUMN min_score smallint,
- MODIFY COLUMN max_score smallint,
  ADD COLUMN cut_point_4 smallint,
  ADD COLUMN cut_point_5 smallint;
 
 ALTER TABLE asmt
  -- allow nulls
  MODIFY COLUMN cut_point_2 smallint,
- MODIFY COLUMN min_score smallint,
- MODIFY COLUMN max_score smallint,
  ADD COLUMN cut_point_4 smallint,
  ADD COLUMN cut_point_5 smallint;
--- warehouse/sql/V1_2_1_2__asmt_cut_points.sql
+++ warehouse/sql/V1_2_1_2__asmt_cut_points.sql
@@ -4,7 +4,5 @@ use ${schemaName};
 ALTER TABLE asmt_score
 -- allow nulls
  MODIFY COLUMN cut_point_2 float,
- MODIFY COLUMN min_score float,
- MODIFY COLUMN max_score float,
  ADD COLUMN cut_point_4 float,
  ADD COLUMN cut_point_5 float;
