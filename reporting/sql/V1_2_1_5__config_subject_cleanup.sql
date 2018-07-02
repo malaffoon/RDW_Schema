0b2bae04de9459d349ac8119dc8796bd2134f1cc
Alla Gorina <agorina@fairwaytech.com>
2018-07-02 10:00:13 -0700

    Remove unused columns

--- reporting/sql/V1_2_1_5__config_subject_cleanup.sql
+++ reporting/sql/V1_2_1_5__config_subject_cleanup.sql
@@ -1,3 +1,4 @@
+-- Remove unused columns
 use ${schemaName};
 
 ALTER TABLE depth_of_knowledge
@@ -14,9 +15,9 @@ ALTER TABLE subject_claim_score
     DROP COLUMN name;
 
 ALTER TABLE target
-    DROP COLUMN code,
-    DROP COLUMN description,
-    MODIFY COLUMN natural_id varchar(20) not null;
+   DROP COLUMN code,
+   DROP COLUMN description,
+   MODIFY COLUMN natural_id varchar(20) not null;
 
 ALTER TABLE grade
    DROP COLUMN name;
@@ -28,4 +29,24 @@ ALTER TABLE item
    DROP COLUMN target_code,
    DROP COLUMN dok_level_subject_id;
 
--- TODO: modify staging tables
+-- Modify corresponding staging tables
+ALTER TABLE staging_depth_of_knowledge
+    DROP COLUMN description;
+
+ALTER TABLE staging_claim
+    DROP COLUMN name,
+    DROP COLUMN description;
+
+ALTER TABLE staging_common_core_standard
+    DROP COLUMN description;
+
+ALTER TABLE staging_subject_claim_score
+    DROP COLUMN name;
+
+ALTER TABLE staging_target
+    DROP COLUMN code,
+    DROP COLUMN description,
+    MODIFY COLUMN natural_id varchar(20) not null;
+
+ALTER TABLE staging_grade
+    DROP COLUMN name;
