db3966a450c41efb104d5f8c2f1b8ba974dce905
Mark Laffoon <laffoon@san.rr.com>
2019-01-10 08:16:49 -0800

    remove "extra" languages; risk of keeping them isn't worth it

--- warehouse/sql/V1_3_0_4__language_order.sql
+++ warehouse/sql/V1_3_0_4__language_order.sql
@@ -1,4 +1,5 @@
 -- Add display order for languages
+-- Remove all languages without an altcode / display order.
 
 USE ${schemaName};
 
@@ -8,3 +9,4 @@ ALTER TABLE language
 UPDATE language
   SET display_order = CASE WHEN altcode = 'UU' THEN 99 ELSE CAST(altcode AS UNSIGNED) END
   WHERE altcode IS NOT NULL;
+DELETE FROM language WHERE display_order IS NULL;
