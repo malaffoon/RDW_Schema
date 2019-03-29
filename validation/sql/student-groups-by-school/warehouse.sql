b9e5cf555d1b0d82ffd9cea84d5a527b53139a37
Mark Laffoon <laffoon@san.rr.com>
2019-03-29 16:03:22 -0700

    review feedback: check active flag when counting student groups

--- validation/sql/student-groups-by-school/warehouse.sql
+++ validation/sql/student-groups-by-school/warehouse.sql
@@ -2,5 +2,5 @@ SELECT d.name, d.natural_id, s.name, s.natural_id, count(*) AS count
 FROM student_group sg
   JOIN school s on sg.school_id = s.id
   JOIN district d on s.district_id = d.id
-WHERE sg.deleted = 0
+WHERE sg.deleted = 0 and active = 1
 GROUP BY sg.school_id;
--- validation/sql/student-groups-by-year/warehouse.sql
+++ validation/sql/student-groups-by-year/warehouse.sql
@@ -1,4 +1,4 @@
 SELECT school_year, count(*) AS count
 FROM student_group
-WHERE deleted = 0
+WHERE deleted = 0 and active = 1
 GROUP BY school_year;
