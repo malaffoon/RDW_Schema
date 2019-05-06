ad4c6d33cece2e42a36470decd84654cd2b34917
esotrope <llunsford@fairwaytech.com>
2019-05-06 15:07:55 -0700

    adds ingest pipeline transformation related tables to warehouse schema

---
+++ warehouse/sql/V1_4_0_4__pipeline.sql
@@ -0,0 +1,37 @@
+-- Adds pipeline, pipeline script and pipeline test tables used for managing ingest pipeline transformations
+
+use ${schemaName};
+
+CREATE TABLE IF NOT EXISTS pipeline (
+  id tinyint NOT NULL PRIMARY KEY,
+  code varchar(100) NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS pipeline_script (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  pipeline_id tinyint NOT NULL,
+  body text NOT NULL,
+  created timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  updated timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  updated_by varchar(255) NOT NULL,
+  INDEX idx__pipeline_script__pipeline_id (pipeline_id),
+  CONSTRAINT fk__pipeline_script__pipeline_id FOREIGN KEY (pipeline_id) REFERENCES pipeline(id)
+);
+
+CREATE TABLE IF NOT EXISTS pipeline_test (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  pipeline_id tinyint NOT NULL,
+  name varchar(500),
+  actual_input text NOT NULL,
+  expected_output text NOT NULL,
+  created timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  updated timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
+  updated_by varchar(255) NOT NULL,
+  INDEX idx__pipeline_test__pipeline_id (pipeline_id),
+  CONSTRAINT fk__pipeline_test__pipeline_id FOREIGN KEY (pipeline_id) REFERENCES pipeline(id)
+);
+
+INSERT INTO pipeline (id, code) VALUES
+  (1, 'assessment'),
+  (1, 'exam'),
+  (1, 'group');
