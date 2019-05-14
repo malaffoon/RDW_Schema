17301fd2a3ea5b9fc587fbbd443dec0b58d415ba
esotrope <llunsford@fairwaytech.com>
2019-05-14 16:45:02 -0700

    removes population of empty scripts

--- warehouse/sql/V1_4_0_5__pipeline_input_type.sql
+++ warehouse/sql/V1_4_0_5__pipeline_input_type.sql
@@ -1,13 +1,9 @@
--- Adds input type column to pipeline table
--- Sets pipeline input types
--- Adds default empty script for each pipeline
+-- Adds input type column to pipeline table and sets each pipelines input types
 
 use ${schemaName};
 
 ALTER TABLE pipeline
   ADD COLUMN input_type varchar(20);
-
--- set pipline input types
 
 UPDATE pipeline
   SET input_type = 'xml'
@@ -16,10 +12,3 @@ WHERE code in ('exam');
 UPDATE pipeline
   SET input_type = 'csv'
 WHERE code in ('group', 'assessment');
-
--- provide pipelines with an empty script
-
-INSERT INTO pipeline_script (pipelineId, body, updated_by) VALUES
- ((SELECT id FROM pipeline WHERE code = 'exam'), '', 'System'),
- ((SELECT id FROM pipeline WHERE code = 'group'), '', 'System'),
- ((SELECT id FROM pipeline WHERE code = 'assessment'), '', 'System');
